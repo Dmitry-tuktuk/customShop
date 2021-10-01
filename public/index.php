01850b227ff917bc675ffb715b4c1d02692abb28
Dmitry-tuktuk <tuk.manager@gmail.com>
2021-10-01 14:34:52 +0300

    Create app container and class registry

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 
 require_once dirname(__DIR__) . "./config/init.php";
+require_once LIBS . '/functions.php';
+
+new \shop\App();
+\shop\App::$app->setProperty('param', 'test');
+debug(\shop\App::$app->getProperties());
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'cc396157c75c266c3c2cf943e803e85c693c6d8e',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'cc396157c75c266c3c2cf943e803e85c693c6d8e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
