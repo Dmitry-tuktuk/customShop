394146a6c3bcfa704183293644472338d2026814
Dmitry-tuktuk <tuk.manager@gmail.com>
2021-10-01 11:55:27 +0300

    The folder structure is created and the composer is connected

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'aa6d681c6d42b14868ebb18c7be7f95ef2f30ce0',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'aa6d681c6d42b14868ebb18c7be7f95ef2f30ce0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
