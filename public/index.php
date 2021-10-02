82492cd8cac7e36ad1eca0b493fc7c486eaa7994
Dmitry-tuktuk <57315387+Dmitry-tuktuk@users.noreply.github.com>
2021-10-02 20:50:00 +0300

    Created ErrorHandler

--- public/index.php
+++ public/index.php
@@ -4,6 +4,6 @@ require_once dirname(__DIR__) . "./config/init.php";
 require_once LIBS . '/functions.php';
 
 new \shop\App();
-\shop\App::$app->setProperty('param', 'test');
-debug(\shop\App::$app->getProperties());
+
+throw new Exception('Страница не найдена', 401);
 
---
+++ vendor/shop/core/ErrorHandler.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace shop;
+
+class ErrorHandler
+{
+    public function __construct()
+    {
+        if(DEBUG){
+            error_reporting(-1);
+        } else {
+            error_reporting(0);
+        }
+        set_exception_handler([$this, 'exceptionHandler']);
+    }
+
+    public function exceptionHandler($exceptions)
+    {
+        $this->logErrors($exceptions->getMessage(), $exceptions->getFile(), $exceptions->getLine());
+        $this->displayError('Исключения', $exceptions->getMessage(), $exceptions->getFile(), $exceptions->getLine(), $exceptions->getCode());
+    }
+
+    protected function logErrors($message = '', $file = '', $line = '')
+    {
+        error_log("[". date('Y-m-d H:i:s') . "] Текст ошибки: {$message} | Файл: {$file} | Строка: {$line} \n===============-\n",
+            3, ROOT . '/tmp/errors.log' );
+    }
+
+    protected function displayError($errorNum, $errorStr, $errorFile, $errorLine, $response = 404 )
+    {
+        http_response_code($response);
+        if ($response == 404 && !DEBUG){
+            require_once WWW . '/errors/404.php';
+            die;
+        }
+        if (DEBUG){
+            require_once WWW . '/errors/dev.php';
+        }
+        else {
+            require_once WWW . '/errors/prod.php';
+        }
+        die;
+    }
+
+
+
+
+}
