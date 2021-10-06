465fec6ffe2230d259aa4af8af5c721f32b5e6dc
Dmitry-tuktuk <tuk.manager@gmail.com>
2021-10-06 12:06:12 +0300

    Template connections in my framework

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -11,25 +11,9 @@ class MainController extends AppController
 
     public function indexAction(){
 
-        $posts = R::findAll('test');
-
-        $post = R::findOne('test', 'id = ?', [2]);
-
         $this->setMeta(App::$app->getProperty('shop_name'), 'описание', 'ключи');
 
-        $name = 'Test';
-        $age = 'Test23';
-        $params = ['name', 'email', 'password', 'desc'];
+        //Как передать другой вид стр ?
 
-        $cache = Cache::instance();
-        //$cache->delete('test');
-        $data = $cache->get('test');
-        if (!$data){
-            $cache->set('test', $params);
-        }
-
-        debug($data);
-
-        $this->set(compact('name', 'age', 'params', 'posts'));
     }
 }
