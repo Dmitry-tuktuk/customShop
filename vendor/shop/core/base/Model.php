27c71ced53414f8e715c595290055273c8d90591
Dmitry-tuktuk <tuk.manager@gmail.com>
2021-11-09 11:41:50 +0200

    Part 2. Connecting the validator and displaying messages

--- vendor/shop/core/base/Model.php
+++ vendor/shop/core/base/Model.php
@@ -3,6 +3,7 @@
 namespace shop\base;
 
 use shop\Db;
+use Valitron\Validator;
 
 abstract class Model extends \RedBeanPHP\SimpleModel {
 
@@ -20,5 +21,28 @@ abstract class Model extends \RedBeanPHP\SimpleModel {
                 $this->attributes[$name] = $data[$name];
             }
         }
+    }
+
+    public function validate($data){
+        Validator::langDir(WWW.'/validator/lang');
+        Validator::lang('ru');
+        $v = new Validator($data);
+        $v->rules($this->rules);
+        if ($v->validate()){
+            return true;
+        }
+        $this->errors = $v->errors();
+        return false;
+    }
+
+    public function getErrors(){
+        $errors = '<ul>';
+        foreach ($this->errors as $error) {
+            foreach ($error as $item){
+                $errors .= "<li>$item</li>";
+            }
+        }
+        $errors .= '</ul>';
+        $_SESSION['error'] = $errors;
     }
 }
